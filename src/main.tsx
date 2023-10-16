5a6d39583fd10022528652c113f0043fe89d8e33
PedroBozzeta <108239149+PedroBozzeta@users.noreply.github.com>
2023-10-16 16:00:26 -0500

    Adding Sass and Bootstrap

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 
+import "@popperjs/core";
+import "bootstrap";
+
+import "./core/styles/app.scss";
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
 		<App />
